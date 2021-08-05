4bbb03e88e6e9929dd0fdc4835734c0da7acb1f3
mdmasum-shuvo <mdmasum.shuvo.cse@gmail.com>
2021-08-06 00:46:26 +0600

    commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,175 @@
+package com.example.myapplication
+
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.provider.Settings
+import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.Snackbar.make
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var progress: Progress? = null
+    private var isLoaded: Boolean = false
+    private var doubleBackToExitPressedOnce = false
+    private var webURL = "https://www.linkedin.com/" // Change it with your URL
+
+    private lateinit var webView: WebView
+    private lateinit var refresh: SwipeRefreshLayout
+    private lateinit var noInternet: RelativeLayout
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        webView = findViewById<WebView>(R.id.webView)
+        refresh = findViewById<SwipeRefreshLayout>(R.id.refresh)
+        noInternet = findViewById<RelativeLayout>(R.id.no_connection)
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.allowFileAccess = true
+        settings.domStorageEnabled = true
+        settings.javaScriptCanOpenWindowsAutomatically = true
+        settings.supportMultipleWindows()
+        if (!isNetworkAvailable()) {
+            noInternet.visibility = View.VISIBLE
+        }
+        loadWebView()
+
+        refresh.setOnRefreshListener {
+            webView.reload()
+        }
+    }
+
+
+    private fun loadWebView() {
+        showProgress(true)
+        webView.loadUrl(webURL)
+        webView.webViewClient = object : WebViewClient() {
+            @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                val url = request?.url.toString()
+                if (url.startsWith("tel:") || url.startsWith("whatsapp:")) {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                    startActivity(intent)
+                    webView.goBack()
+                    return true
+                } else {
+                    view?.loadUrl(url)
+                }
+                return super.shouldOverrideUrlLoading(view, request)
+            }
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                showProgress(true)
+                noInternet.visibility = View.GONE
+                super.onPageStarted(view, url, favicon)
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                isLoaded = true
+                showProgress(false)
+                super.onPageFinished(view, url)
+            }
+
+            override fun onReceivedError(
+                view: WebView,
+                request: WebResourceRequest,
+                error: WebResourceError
+            ) {
+                isLoaded = false
+                showProgress(false)
+                if (!isNetworkAvailable()) {
+                    noInternet.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        if (event.action == KeyEvent.ACTION_DOWN) {
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    showToastToExit()
+                }
+                return true
+            }
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    private fun showToastToExit() {
+        when {
+            doubleBackToExitPressedOnce -> {
+                onBackPressed()
+            }
+            else -> {
+                doubleBackToExitPressedOnce = true
+                showToast(getString(R.string.back_again_to_exit))
+                Handler(Looper.myLooper()!!).postDelayed(
+                    { doubleBackToExitPressedOnce = false },
+                    2000
+                )
+            }
+        }
+    }
+
+    private fun showProgress(visible: Boolean) {
+        refresh.isRefreshing = visible
+    }
+
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showNoNetSnackBar() {
+        val snack =
+            Snackbar.make(
+                findViewById(R.id.rootView),
+                getString(R.string.no_internet),
+                Snackbar.LENGTH_INDEFINITE
+            )
+        snack.setAction(getString(R.string.settings)) {
+            startActivity(Intent(Settings.ACTION_WIFI_SETTINGS))
+        }
+        snack.show()
+    }
+
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetworkInfo = connectivityManager.activeNetworkInfo
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected
+    }
+
+}
